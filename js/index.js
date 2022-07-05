c743b0cfa3ab3936da00e8ff3b4bfc5ce3d7b332
Anderson Daniel <84481208+antiderson@users.noreply.github.com>
2022-07-05 14:15:48 -0300

    .response

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// modais
+function showModal() {
+    var element = document.getElementById("modal");
+    element.classList.add("show-modal");
+}
+
+function closeModal() {
+    var element = document.getElementById("modal");
+    element.classList.remove("show-modal");
+}
+
+function showModal2() {
+    var element = document.getElementById("modal2");
+    element.classList.add("show-modal");
+}
+
+function closeModal2() {
+    var element = document.getElementById("modal2");
+    element.classList.remove("show-modal");
+}
